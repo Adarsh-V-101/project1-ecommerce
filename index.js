3006c6b9b5caca2fbbe32c44a4f2f6593df54741
DrShambala <adarshvishwakarma504@gmail.com>
2025-09-10 20:33:34 +0530

    new start

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv'
 import connectDb from './config/db.js';
 import userRoute from './routes/userRoute.js'
-import orderRoute from './routes/orderRoute.js'
-import productRoute from './routes/productRoute.js'
 
 dotenv.config()
 connectDb()
@@ -12,8 +10,6 @@ const app = express()
 app.use(express.json())
 
 app.use('/api/users', userRoute)
-app.use('/api/product', productRoute)
-app.use('/api/order',orderRoute)
 
 app.get('/', (req,res )=>{
     console.log('working');
